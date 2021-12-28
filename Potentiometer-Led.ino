8c96aabae114b1d27c4f6052cdb23e15f08bb18e
bulentsezen <50150555+bulentsezen@users.noreply.github.com>
2021-12-28 17:41:06 +0300

    Add files via upload

---
+++ Potentiometer-Led.ino
@@ -0,0 +1,20 @@
+#include <cvzone.h>
+
+#define led 3
+
+SerialData serialData;
+
+int sendVals[2]; // min val of 2 even when sending 1
+
+void setup() {
+serialData.begin(9600);
+}
+
+void loop() {
+  int potVal = analogRead(A0);
+  sendVals[0] = potVal;
+  serialData.Send(sendVals);
+
+  int deger = potVal / 4;
+  analogWrite(led,deger);
+}
